02925ba0efecc474d05db9be4fbe420729450e8c
maichomper <maichomper@gmail.com>
2015-12-26 16:09:29 -0600

    taxonomy terms para recursos

---
+++ themes/proparty/single-resource.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+
+
+
+				
+<div class="content_wrap">
+<div class="content">
+<article class="itemscope post_item post_item_single post_featured_default post_format_standard post-3348 resource type-resource status-publish has-post-thumbnail hentry language-english sector-agriculture country-mexico resource_type-datasets working_group-incentive-mechanisms" itemscope itemtype="http://schema.org/Article">			<section class="post_featured">
+							<div class="post_thumb" data-image="http://localhost:8888/odc/wp-content/uploads/2015/12/odc_cover_web-06.png" data-title="Some Resource">
+					<a class="hover_icon hover_icon_view" href="http://localhost:8888/odc/wp-content/uploads/2015/12/odc_cover_web-06.png" title="Some Resource"><img class="wp-post-image" width="1150" height="647" alt="Some Resource" src="http://localhost:8888/odc/wp-content/uploads/2015/12/odc_cover_web-06-1150x647.png" itemprop="image"></a>
+				</div>
+							</section>
+			
+<section class="post_content" itemprop="articleBody"><p>Lorem ipsum&#8230;.</p>
+				</section> <!-- </section> class="post_content" itemprop="articleBody"> -->	<section class="related_wrap related_wrap_empty"></section>
+	
+			</article> <!-- </article> class="itemscope post_item post_item_single post_featured_default post_format_standard post-3348 resource type-resource status-publish has-post-thumbnail hentry language-english sector-agriculture country-mexico resource_type-datasets working_group-incentive-mechanisms" itemscope itemtype="http://schema.org/Article"> --><!-- Post/page views count increment -->
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		jQuery.post(AXIOM_GLOBALS['ajax_url'], {
+			action: 'post_counter',
+			nonce: AXIOM_GLOBALS['ajax_nonce'],
+			post_id: 3348,
+			views: 1		});
+	});
+</script>
+
+		</div> <!-- </div> class="content"> -->	<div class="sidebar widget_area bg_tint_light sidebar_style_light" role="complementary">
+		<aside id="search-6" class="widget_number_1 widget widget_search"><form role="search" method="get" class="search_form" action="http://localhost:8888/odc/"><input type="text" class="search_field" placeholder="Search &hellip;" value="" name="s" title="Search for:" /><button type="submit" class="search_button icon-search-1" href="#"></button></form></aside>		<aside id="recent-posts-4" class="widget_number_2 widget widget_recent_entries">		<h5 class="widget_title">Recent Posts</h5>		<ul>
+					<li>
+				<a href="http://localhost:8888/odc/leading-global-businesses-policymakers-share-commitment-to-work-with-un-to-enable-a-data-revolution-for-climate-action/">Leading Global Businesses, Policymakers Share Commitment To Work with UN to Enable a Data Revolution for Climate Action</a>
+						</li>
+					<li>
+				<a href="http://localhost:8888/odc/the-consultation-on-g20-anti-corruption-package-is-launched/">The Consultation of the AntiCorruption Package is launched at G20 Summit</a>
+						</li>
+					<li>
+				<a href="http://localhost:8888/odc/seventeen-governments-adopt-the-new-international-open-data-charter/">Seventeen Governments Adopt the New International Open Data Charter</a>
+						</li>
+					<li>
+				<a href="http://localhost:8888/odc/openness-as-an-enabler-of-the-2030-sustainable-development-agenda/">Openness as an enabler of the 2030 Sustainable Development Agenda</a>
+						</li>
+					<li>
+				<a href="http://localhost:8888/odc/a-global-call-to-adopt-the-open-data-charter/">A global call to adopt the Open Data Charter</a>
+						</li>
+				</ul>
+		</aside>			</div> <!-- /.sidebar -->
+	
+	</div> <!-- </div> class="content_wrap"> -->			
+
+<?php get_footer(); ?>

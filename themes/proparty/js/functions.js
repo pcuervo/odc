7b09c803896b791559c7278a6899cae3c958916c
maichomper <maichomper@gmail.com>
2015-12-26 11:22:40 -0600

    agregar isotope, taxonomies y post type resources

---
+++ themes/proparty/js/functions.js
@@ -0,0 +1,72 @@
+var $=jQuery.noConflict();
+
+/**
+ * Run Isotope plugin
+ * @container element cointaining items
+ * @item element inside the container
+**/
+function runIsotope( container, item ){
+    var $container = $(container);
+    $container.imagesLoaded( function(){
+        $container.isotope({
+            itemSelector : item,
+            layoutMode: 'masonry',
+            masonry: {
+                columnWidth: item
+            }
+        });
+    });
+}// runIsotope
+
+
+/**
+ * Filster in Isotope plugin
+ * @container element cointaining items
+ * @item element inside the container
+**/
+function filterIsotope( container, item ){
+    var $grid = $(container);
+    $grid.imagesLoaded( function(){
+        $grid.isotope({
+            itemSelector : item,
+            layoutMode: 'masonry',
+            masonry: {
+                columnWidth: item
+            }
+        });
+    });
+
+    // store filter for each group
+    var filters = {};
+
+    $('.filtros').on( 'click', '.button-group a', function() {
+        var $this = $(this);
+        console.log($this);
+        // get group key
+        var $buttonGroup = $this.parents('.button-group');
+        var filterGroup = $buttonGroup.attr('data-filter-group');
+        // set filter for group
+        filters[ filterGroup ] = $this.attr('data-filter');
+        // combine filters
+        var filterValue = concatValues( filters );
+        // set filter for Isotope
+        $grid.isotope({ filter: filterValue });
+    });
+
+    // add an active class to active filters
+    $('.button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'a', function( e ) {
+            e.preventDefault();
+            $buttonGroup.find('.active').removeClass('active');
+            $( this ).addClass('active');
+        });
+    });
+}// filterIsotope
+
+// flatten object by concatting values
+function concatValues( obj ) {
+    var value = '';
+    for ( var prop in obj ) value += obj[ prop ];
+    return value;
+}
